4578452a14c9742752d30933eb8ed276c7e6d3c3
lrhyde <lrhyde13@gmail.com>
2023-12-04 00:04:22 -0600

    speedy day 4 :)

--- day4.py
+++ day4.py
@@ -6,14 +6,22 @@ def parseNumList(numList):
         if(num!=""): 
             newList.append(int(num))
     return newList
-
+points = dict()
+copies = dict()
 sum = 0
-for line in infile:
-    line = line.split(": ")[1]
+for line_full in infile:
+    lineNum = int(line_full.split(": ")[0].strip().split(" ")[len(line_full.split(": ")[0].strip().split(" "))-1])
+    copies[lineNum]=1
+    points[lineNum]=0
+    line = line_full.split(": ")[1]
     actualNums = parseNumList(line.split(" | ")[0].strip().split(" "))
     winningNums = parseNumList(line.split(" | ")[1].strip().split(" "))
     totalNums = actualNums + winningNums
     totalSet = set(totalNums)
-    if(len(totalNums)!=len(totalSet)):
-        sum+=2**((len(totalNums)-len(totalSet))-1)
+    points[lineNum] = len(totalNums)-len(totalSet)
+for i in range(1, 203):
+    for j in range(i+1, i+1+points[i]):
+        copies[j]+=copies[i]
+for i in range(1, 203):
+    sum+=copies[i]
 print(sum)
