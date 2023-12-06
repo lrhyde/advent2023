d0a1f82a2774e44c64709528a29ad31b6b3cfb56
lrhyde <lrhyde13@gmail.com>
2023-12-06 13:34:51 -0600

    out of order LOL oopsies

---
+++ day6.py
@@ -0,0 +1,24 @@
+infile = open("day6.txt")
+def parseNumList(numList):
+    newList = []
+    for num in numList:
+        if(num!=""): 
+            newList.append(int(num))
+    return newList
+timeList = []
+distList = []
+for line in infile:
+    if(line[0]=="T"):
+        timeList= parseNumList(line.split(" ")[1:])
+    if(line[0]=="D"):
+        distList  = parseNumList(line.split(" ")[1:])
+successes = []
+for i in range(len(timeList)):
+    successes.append(0)
+    for j in range(timeList[i]):
+        if(j*(timeList[i]-j)>distList[i]):
+            successes[i]+=1
+sum = 1
+for i in range(len(timeList)):
+    sum*=successes[i]
+print(sum)
