e8cdd0e91fab06949b1cb5bceabd846ccdcd8a3f
lrhyde <lrhyde13@gmail.com>
2023-12-11 13:56:37 -0600

    super quick part 2?

--- day7.py
+++ day7.py
@@ -1,15 +1,19 @@
 infile = open("day7.txt")
 bids = []
-cardOrder = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2']
+cardOrder = ['A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2', 'J']
 sum = 0
 def evalCard(card):
     sCard = list(set(card))
     vals = []
     for c in sCard:
-        vals.append(card.count(c))
+        if(c!='J'):
+            vals.append(card.count(c))
     vals = sorted(vals)
     vals.reverse()
-    score = vals[0]*(14**6)
+    jokerCount = card.count('J')
+    if(len(vals)==0):
+        vals.append(0)
+    score = (vals[0]+jokerCount)*(14**6)
     if(len(vals)>1):
         score+=vals[1]*(14**5)
     for i in range(len(card)):
