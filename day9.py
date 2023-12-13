90da87e4d45843b315542f3a19bd6882797228c3
lrhyde <lrhyde13@gmail.com>
2023-12-12 19:17:33 -0600

    see! catching up

---
+++ day9.py
@@ -0,0 +1,27 @@
+infile = open("day9.txt")
+sum = 0
+def parseNumList(numList):
+    newList = []
+    for num in numList:
+        if(num!=""): 
+            newList.append(int(num))
+    return newList
+def extrapolate(sequence):
+    complete = False
+    differences = []
+    differences.append(sequence)
+    while(not complete):
+        differences.append([differences[len(differences)-1][i+1]-differences[len(differences)-1][i] for i in range(len(differences[len(differences)-1])-1)])
+        if(differences[len(differences)-1] == [0 for n in differences[len(differences)-1]]):
+            complete = True
+    prevDiff = 0
+    for i in range(len(differences)):
+        iReverse = len(differences)-i-1
+        value = differences[iReverse][len(differences[iReverse])-1] + prevDiff
+        prevDiff = value
+    return value
+for line in infile:
+    numLine = parseNumList(line.split(" "))
+    ex = extrapolate(numLine)
+    sum+=ex
+print(sum)
