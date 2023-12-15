f5e86b9e68ff4067f33ea0087041fc911dec61a6
lrhyde <lrhyde13@gmail.com>
2023-12-15 17:25:26 -0600

    honestly they keep giving us the answers

--- day15.py
+++ day15.py
@@ -8,4 +8,27 @@ def hash(item):
     return val
 for line in infile:
     inputs = line.strip().split(",")
-    print(sum([hash(input) for input in inputs]))
+    grid = [[] for i in range(256)]
+    for input in inputs:
+        if("-" in input):
+            label = input[0:input.index("-")]
+            box = hash(label)
+            for element in grid[box]:
+                if(element[0]==label):
+                    grid[box].remove(element)
+        else:
+            label = input[0:input.index("=")]
+            box = hash(label)
+            found = False
+            for e in range(len(grid[box])):
+                element = grid[box][e]
+                if(element[0]==label):
+                    grid[box][e] = (label, int(input[input.index("=")+1:]))
+                    found = True
+            if(not found):
+               grid[box].append((label, int(input[input.index("=")+1:])))
+sum = 0
+for i in range(len(grid)):
+    for j in range(len(grid[i])):
+        sum+=(i+1)*(j+1)*grid[i][j][1]
+print(sum)
